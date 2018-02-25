a9feafc424db3b3ac5a03f361328dfe530d237ad
amanjosan2008 <31513195+amanjosan2008@users.noreply.github.com>
2018-02-25 22:53:43 +0530

    Update video_mgr.py

--- video_mgr.py
+++ video_mgr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+# Fix Button Sizes
+# Separate Window for Directory Operations
 
-#import vlc
 import os,sys
 import time
 try:
@@ -8,7 +9,7 @@ try:
     from tkinter import *
     import tkinter
 except:
-    from Tkinter import * 
+    from Tkinter import *
 import subprocess
 import shutil
 from send2trash import send2trash
@@ -19,7 +20,6 @@ try:
 
     # Variables
     array,playlist,MODES = [],[],[]
-    #global song
     m,d = 0,0
 
     # Populate Directory List
@@ -27,11 +27,20 @@ try:
         array = [line.rstrip('\n') for line in open('dirlist.ini')]
     except FileNotFoundError:
         print("Error: dirlist.ini not found. Please create the file.")
-        #sys.exit()
         root.quit()
 
     for i in range(len(array)):
         MODES.append(array[i].split('\t'))
+
+    def filesize(file):
+        size = os.path.getsize(file)
+        sizeinmb = size/1000000
+        sizeflt = "{:.2f}".format(sizeinmb)
+        return sizeflt
+
+    def lb(text):
+        listbox.insert(END, text)
+        listbox.yview(END)
 
     # Various Functions
     def browse():
@@ -48,53 +57,63 @@ try:
             for filename in os.listdir(en.get()):
                     if os.path.isfile(os.path.join(en.get(), filename)):
                         playlist.append(os.path.join(en.get(), filename))
-            listbox.insert(END, "Total no of Files: "+str(len(playlist)))
+            lb("Total no of Files: "+str(len(playlist)))
         except FileNotFoundError:
-            listbox.insert(END, "Error: Directory not selected")
+            lb("Error: Directory not selected")
+        lb("")
 
     def openfolder():
         if en.get():
             path = 'nautilus "%s"' %en.get()
             subprocess.Popen(path, shell=True)
+            lb("Directory opened: "+en.get())
         else:
-            listbox.insert(END, "Error: Directory not selected")
+            lb("Error: Directory not selected")
+        lb("")
 
-    def plist():
+    def ls_dir():
         if en.get():
             count = [name for name in os.listdir(en.get()) if os.path.isfile(os.path.join(en.get(), name))]
             if (len(count)==0):
-                listbox.insert(END, "Error: No Files in Directory")
+                lb("Error: No Files in Directory")
             else:
+                lb("File List:")
                 for i in range(len(count)):
-                    listbox.insert(END, count[i])
+                    lb(str(i+1)+ ". " + count[i] + " " + "["+ filesize(en.get()+"/"+count[i]) + "MB" + "]")
         else:
-            listbox.insert(END, "Error: Directory not selected")
+            lb("Error: Directory not selected")
+        lb("")
 
     def stats():
         try:
             count = len([name for name in os.listdir(en.get()) if os.path.isfile(os.path.join(en.get(), name))])
         except:
             count = "No Files"
-        listbox.insert(END, "Initial Count: "+str(len(playlist)))
-        listbox.insert(END, "Current Count: "+str(count))
-        listbox.insert(END, "Deleted: "+str(d))
-        listbox.insert(END, "Moved: "+str(m))
+        lb("File Operation Stats:")
+        lb("Initial Count: "+str(len(playlist)))
+        lb("Current Count: "+str(count))
+        lb("Deleted: "+str(d))
+        lb("Moved: "+str(m))
+        lb("")
 
     def play(delta):
         global current
         if en.get():
             if not (0 <= current + delta < len(playlist)):
-                listbox.insert(END, "End of List")
+                lb("End of List")
+                lb("")
                 return
             current += delta
             song = 'vlc -q "%s" 2> /dev/null' %playlist[current]
             if os.path.isfile(playlist[current]):
-                listbox.insert(END, str(current+1)+": "+"VLC: "+playlist[current])
+                lb(str(current+1)+": "+"VLC: "+playlist[current]+ " " + "["+ filesize(playlist[current]) + "MB" + "]")
                 subprocess.Popen(song, shell=True)
             else:
-                listbox.insert(END, "Error: File not found: "+(playlist[current]).split('/')[-1])
+                lb("Error: File not found: "+(playlist[current]).split('/')[-1])
+                lb("")
         else:
-            listbox.insert(END, "Error: Directory not selected")
+            lb("Error: Directory not selected")
+            lb("")
 
     def move():
         if en.get():
@@ -102,57 +121,75 @@ try:
                 try:
                     if os.path.isfile(playlist[current]):
                         shutil.move(playlist[current], v.get())
-                        listbox.insert(END, "Moved: "+playlist[current]+" => "+v.get())
+                        lb("Moved: "+playlist[current]+" => "+v.get())
                         global m
                         m += 1
                         play(+1)
                     else:
-                        listbox.insert(END, "Error: Source file does not exist: "+(playlist[current]).split('/')[-1])
+                        lb("Error: Source file does not exist: "+(playlist[current]).split('/')[-1])
                 except shutil.Error:
-                    listbox.insert(END, "Error: "+(playlist[current]).split('/')[-1]+": already exists at destination: "+v.get())
+                    lb("Error: "+(playlist[current]).split('/')[-1]+": already exists at destination: "+v.get())
                 except FileNotFoundError:
-                    listbox.insert(END, "Error: File not found: "+(playlist[current]).split('/')[-1])
+                    lb("Error: File not found: "+(playlist[current]).split('/')[-1])
             else:
-                listbox.insert(END, "Error: Directory does not exist: "+v.get())
+                lb("Error: Directory does not exist: "+v.get())
         else:
-            listbox.insert(END, "Error: Directory not selected")
+            lb("Error: Directory not selected")
+        lb("")
 
     def movedir():
         if en.get():
             if os.path.isdir(v.get()):
                 try:
                     shutil.move(en.get(), v.get())
-                    listbox.insert(END, "Moved: "+en.get()+" => "+v.get())
+                    lb("Moved: "+en.get()+" => "+v.get())
                 except shutil.Error:
-                    listbox.insert(END, "Error: "+en.get()+": already exists at destination: "+v.get())
+                    lb("Error: "+en.get()+": already exists at destination: "+v.get())
             else:
-                listbox.insert(END, "Error: Directory does not exist: "+v.get())
+                lb("Error: Directory does not exist: "+v.get())
         else:
-            listbox.insert(END, "Error: Directory not selected")
+            lb("Error: Directory not selected")
+        lb("")
 
     def delete():
         if en.get():
             send2trash(playlist[current])
-            listbox.insert(END, "Deleted: "+playlist[current])
+            lb("Deleted: "+playlist[current])
             global d
             d += 1
             play(+1)
         else:
-            listbox.insert(END, "Error: Directory not selected")
+            lb("Error: Directory not selected")
+        lb("")
 
     def deleteall():
         if en.get():
-            listbox.insert(END, "Deleted All files: ")
+            lb("Deleted All files: ")
             for i in range(len(playlist)):
                  try:
                      send2trash(playlist[i])
-                     listbox.insert(END, " -  "+playlist[i])
+                     lb(" -  "+playlist[i])
                      global d
                      d += 1
                  except:
-                     listbox.insert(END, " -  "+"Already Deleted: "+playlist[i])
+                     lb(" -  "+"Already Deleted: "+playlist[i])
         else:
-            listbox.insert(END, "Error: Directory not selected")
+            lb("Error: Directory not selected")
+        lb("")
+
+    def del_dir():
+        if en.get():
+            try:
+                if os.listdir(en.get()) == []:
+                    os.rmdir(en.get())
+                    lb("Directory Deleted: "+ en.get())
+                else:            
+                    lb("Directory not empty: Contains "+ str(len(os.listdir(en.get()))) + " files")
+            except FileNotFoundError:
+                lb("Error: Directory not found, probably already deleted")
+        else:
+            lb("Error: Directory not selected")
+        lb("")
 
     def clear():
         listbox.delete(0, END)
@@ -176,9 +213,10 @@ try:
             f = open('dirlist.ini','a')
             f.write(en2.get()+"\t"+en3.get()+"\r")
             f.close()
-            listbox.insert(END, "Saved Directory: "+en2.get()+" "+en3.get())
+            lb("Saved Directory: "+en2.get()+" "+en3.get())
         else:
-            listbox.insert(END, "Error: Directory/Name not selected")
+            lb("Error: Directory/Name not selected")
+        lb("")
 
     def delentry():
         f = open('dirlist.ini','r')
@@ -186,10 +224,10 @@ try:
         if en4.get():
             n = int(en4.get())-1
         else:
-            listbox.insert(END, "Error: Number not entered")
+            lb("Error: Number not entered")
             return
         try:
-            listbox.insert(END, "Removing Listed Directory: " + l[n])
+            lb("Removing Listed Directory: " + l[n])
             line = l[0:n] + l[n+1:]
             f.close()
             f2 = open('dirlist.ini','w')
@@ -197,16 +235,16 @@ try:
                 f2.write(i)
             f2.close()
         except IndexError:
-            listbox.insert(END, "Error: Invalid Number Entered")
-
+            lb("Error: Invalid Number Entered")
+        lb("")
 
     # Buttons Config:
-    Button(root, text='Play',     command=lambda: play(0)).grid(row=0, column=1, rowspan=1, columnspan=1)
-    Button(root, text='Previous', command=lambda: play(-1)).grid(row=0, column=2, rowspan=1, columnspan=1)
-    Button(root, text='Next',     command=lambda: play(+1)).grid(row=0, column=3, rowspan=1, columnspan=1)
-    Button(root, text='List',     command=lambda: plist()).grid(row=0, column=4, rowspan=1, columnspan=1)
-    Button(root, text='Stats',    command=lambda: stats()).grid(row=0, column=5, rowspan=1, columnspan=1)
-    Button(root, text='Clear',    command=lambda: clear()).grid(row=0, column=6, rowspan=1, columnspan=1)
+    Button(root, text='Play',  command=lambda: play(0)).grid(row=0, column=1, rowspan=1, columnspan=1)
+    Button(root, text='Prev', command=lambda: play(-1)).grid(row=0, column=2, rowspan=1, columnspan=1)
+    Button(root, text='Next', command=lambda: play(+1)).grid(row=0, column=3, rowspan=1, columnspan=1)
+    Button(root, text='List',  command=lambda: ls_dir()).grid(row=0, column=4, rowspan=1, columnspan=1)
+    Button(root, text='Stats', command=lambda: stats()).grid(row=0, column=5, rowspan=1, columnspan=1)
+    Button(root, text='Clear', command=lambda: clear()).grid(row=0, column=6, rowspan=1, columnspan=1)
 
     # Browse & Entry Box
     Button(root, text="Browse", command=browse).grid(row=2, column=0, rowspan=1, columnspan=1)
@@ -228,19 +266,24 @@ try:
         pass
 
     # Output Logs Box
-    listbox = Listbox(root, height=20, width=70)
+    scrollbar = Scrollbar(root, orient=VERTICAL)
+    listbox = Listbox(root, height=20, width=70, yscrollcommand=scrollbar.set)
     listbox.xview_scroll(3, "pages")
     listbox.yview_scroll(3, "pages")
+    scrollbar.config(command=listbox.yview)
+    scrollbar.grid(row=3, column=10, rowspan=13, columnspan=1, sticky=N, ipady = 148)
     listbox.grid(row=3, column=1, rowspan=12, columnspan=9)
 
-    listbox.insert(END, "Ready, Log Output: ")
+    lb("Ready, Log Output:")
+    lb("")
 
     # Buttons with file operations
-    Button(root, text='Move', command=lambda: move()).grid(row=3, column=11, rowspan=1, columnspan=1)
-    Button(root, text='Move Dir', command=lambda: movedir()).grid(row=5, column=11, rowspan=1, columnspan=1)
-    Button(root, text='Delete', command=lambda: delete()).grid(row=7, column=11, rowspan=1, columnspan=1)
-    Button(root, text='Delete All', command=lambda: deleteall()).grid(row=11, column=11, rowspan=1, columnspan=1)
-    Button(root, text='Quit', command=exit).grid(row=19, column=11, rowspan=1, columnspan=1)
+    Button(root, text='Move', command=lambda: move()).grid(row=3, column=13, rowspan=1, columnspan=1)
+    Button(root, text='Move Dir', command=lambda: movedir()).grid(row=5, column=13, rowspan=1, columnspan=1)
+    Button(root, text='Delete', command=lambda: delete()).grid(row=7, column=13, rowspan=1, columnspan=1)
+    Button(root, text='Delete All', command=lambda: deleteall()).grid(row=9, column=13, rowspan=1, columnspan=1)
+    Button(root, text='Delete Dir', command=lambda: del_dir()).grid(row=11, column=13, rowspan=1, columnspan=1)
+    Button(root, text='Quit', command=exit).grid(row=19, column=13, rowspan=1, columnspan=1)
 
     # Validate Directories
     try: 
@@ -248,7 +291,8 @@ try:
             if os.path.exists(MODES[i][1]):
                 pass
             else:
-                listbox.insert(END, "Error: Directory does not exist: "+MODES[i][0]+" - "+MODES[i][1])
+                lb("Error: Directory does not exist: "+MODES[i][0]+" - "+MODES[i][1])
+        lb("")
     except:
         pass
 
@@ -269,11 +313,15 @@ try:
 
     Button(root, text="Del Entry", command=lambda: delentry()).grid(row=18, column=2, rowspan=1, columnspan=1)
 
-    root.geometry('800x600')
+    root.geometry('800x520')
     root.title("Video Collection Manager")
-
-    img = PhotoImage(file='icon.png')
-    root.tk.call('wm', 'iconphoto', root._w, img)
+    
+    try:
+        img = PhotoImage(file='icon.png')
+        root.tk.call('wm', 'iconphoto', root._w, img)
+    except:
+        lb("icon.png file not found")
+        
     root.mainloop()
 
 except:
